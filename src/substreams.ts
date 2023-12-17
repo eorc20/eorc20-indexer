901060bed96b191b65d90998c8157d05e4e55268
Denis Carriere <carriere.denis@gmail.com>
2023-12-17 09:55:39 -0500

    add queue

--- src/substreams.ts
+++ src/substreams.ts
@@ -7,6 +7,9 @@ import logUpdate from "log-update";
 import { Hex, fromHex } from "viem";
 import { InscriptionRawData } from "./schemas.js";
 import { handleOpCode } from "./operations/index.js";
+import pQueue from "p-queue";
+
+const queue = new pQueue({ concurrency: 1 });
 
 let operations: string[] = [];
 
@@ -15,82 +18,84 @@ const init_timestamp = Math.floor(Date.now().valueOf() / 1000);
 let last_timestamp = init_timestamp;
 
 emitter.on("anyMessage", async (message: any, cursor, clock) => {
-  if ( !clock.timestamp ) return;
-  const block_number = blockNumberFromGenesis(clock.timestamp?.toDate());
-  const timestamp = Number(clock.timestamp.seconds);
+  queue.add(async () => {
+    if ( !clock.timestamp ) return;
+    const block_number = blockNumberFromGenesis(clock.timestamp?.toDate());
+    const timestamp = Number(clock.timestamp.seconds);
 
-  if (!message.transactionTraces) return;
-  for ( const trace of message.transactionTraces ) {
-    if ( trace.receipt.status !== "TRANSACTIONSTATUS_EXECUTED") return;
-    if ( !trace.actionTraces ) continue;
-    for ( const action of trace.actionTraces) {
-      if ( action.action.name !== "pushtx" ) continue;
-      const jsonData = JSON.parse(action.action.jsonData);
-      // const miner = jsonData.miner;
-      const rlptx: Hex = `0x${jsonData.rlptx}`;
-      const transaction_hash = toTransactionId(rlptx);
-      // const transaction_index = Number(trace.index);
+    if (!message.transactionTraces) return;
+    for ( const trace of message.transactionTraces ) {
+      if ( trace.receipt.status !== "TRANSACTIONSTATUS_EXECUTED") return;
+      if ( !trace.actionTraces ) continue;
+      for ( const action of trace.actionTraces) {
+        if ( action.action.name !== "pushtx" ) continue;
+        const jsonData = JSON.parse(action.action.jsonData);
+        // const miner = jsonData.miner;
+        const rlptx: Hex = `0x${jsonData.rlptx}`;
+        const transaction_hash = toTransactionId(rlptx);
+        // const transaction_index = Number(trace.index);
 
-      // EORC-20 handling
-      const tx = rlptxToTransaction(rlptx);
-      if ( !tx ) continue;
-      const to = tx.to;
-      if ( !to ) continue;
-      if ( !tx.data ) continue;
-      const content = fromHex(tx.data, 'string');
-      const opCode = parseOpCode(content);
-      if ( !opCode ) continue;
-      const from = await getFromAddress(rlptx);
-      if ( !from) continue;
-      // const sha = contentUriToSha256(content);
-      const value = tx.value?.toString();
-      // const gas = tx.gas?.toString();
-      // const gasPrice = tx.gasPrice?.toString();
-      const contentType = getMimeType(content).mimetype;
+        // EORC-20 handling
+        const tx = rlptxToTransaction(rlptx);
+        if ( !tx ) continue;
+        const to = tx.to;
+        if ( !to ) continue;
+        if ( !tx.data ) continue;
+        const content = fromHex(tx.data, 'string');
+        const opCode = parseOpCode(content);
+        if ( !opCode ) continue;
+        const from = await getFromAddress(rlptx);
+        if ( !from) continue;
+        // const sha = contentUriToSha256(content);
+        const value = tx.value?.toString();
+        // const gas = tx.gas?.toString();
+        // const gasPrice = tx.gasPrice?.toString();
+        const contentType = getMimeType(content).mimetype;
 
-      // Write EORC-20 operation to disk used for history
-      operations.push(JSON.stringify({
-        id: transaction_hash,
-        block: block_number,
-        timestamp,
-        from,
-        to,
-        contentType,
-        content,
-        value,
-        // nonce: tx.nonce,
-        // transaction_index,
-        // gas,
-        // gasPrice,
-        // sha,
-        // miner,
-      } as InscriptionRawData) + "\n");
+        // Write EORC-20 operation to disk used for history
+        operations.push(JSON.stringify({
+          id: transaction_hash,
+          block: block_number,
+          timestamp,
+          from,
+          to,
+          contentType,
+          content,
+          value,
+          // nonce: tx.nonce,
+          // transaction_index,
+          // gas,
+          // gasPrice,
+          // sha,
+          // miner,
+        } as InscriptionRawData) + "\n");
 
-      // Update EORC-20 State
-      handleOpCode(transaction_hash, from, to, opCode, timestamp);
+        // Update EORC-20 State
+        handleOpCode(transaction_hash, from, to, opCode, timestamp);
 
-      // Update progress
-      const now = Math.floor(Date.now().valueOf() / 1000);
-      total++;
-      const rate = total / (now - init_timestamp);
-      if ( last_timestamp !== now ) logUpdate(`Processed ${total} EORC-20 operations at ${rate.toFixed(2)} op/s (last block: ${block_number})`);
-      last_timestamp = now
+        // Update progress
+        const now = Math.floor(Date.now().valueOf() / 1000);
+        total++;
+        const rate = total / (now - init_timestamp);
+        if ( last_timestamp !== now ) logUpdate(`Queue ${queue.size} Processed ${total} EORC-20 operations at ${rate.toFixed(2)} op/s (last block: ${block_number})`);
+        last_timestamp = now
+      }
+
+        // Save operations buffer to disk
+        // console.log(`Writing ${operations.length} operations to disk`);
+        writers.eorc20.write(operations.join(""));
+        operations = []; // clear buffer
+
+        // Save cursor
+        saveCursor(cursor);
+        writers.blocks.write(JSON.stringify({
+          timestamp,
+          block_number,
+          eos_block_number: Number(clock.number),
+          eos_block_id: clock.id
+        }) + "\n");
     }
-  }
-
-  // Save operations buffer to disk
-  // console.log(`Writing ${operations.length} operations to disk`);
-  writers.eorc20.write(operations.join(""));
-  operations = []; // clear buffer
-
-  // Save cursor
-  saveCursor(cursor);
-  writers.blocks.write(JSON.stringify({
-    timestamp,
-    block_number,
-    eos_block_number: Number(clock.number),
-    eos_block_id: clock.id
-  }) + "\n");
+  })
 });
 
 // End of Stream
