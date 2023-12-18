d42e964e3eb779eb0ecd09278f52fdf9e872c2e0
Denis Carriere <carriere.denis@gmail.com>
2023-12-18 09:25:05 -0500

    add scan to indexer

--- bin/cli.ts
+++ bin/cli.ts
@@ -1,19 +1,20 @@
 #!/usr/bin/env node
 
-import { PAUSED } from "../src/config.js";
+import { IGNORE_SCAN, PAUSED } from "../src/config.js";
 import { emitter } from "../src/BlockEmitter.js";
 // import { scan } from "../src/scan.js"
 import "../src/substreams.js"
+import { scan } from "../src/scan.js";
 
 if ( PAUSED ) {
     console.log("Paused");
     process.exit(0);
 }
 
-// // Scan existing inscriptions and index them before starting the server
-// if ( !IGNORE_SCAN ) {
-//   await scan();
-// }
+// Scan existing inscriptions and index them before starting the server
+if ( !IGNORE_SCAN ) {
+  await scan();
+}
 
 // Start indexing with substreams
 emitter.start();
--- src/scan.ts
+++ src/scan.ts
@@ -5,6 +5,7 @@ import { client } from "./clickhouse/createClient.js";
 import pQueue from "p-queue";
 import { parseOpCode } from "./eorc20.js";
 
+const max_inserts = 1000;
 const queue = new pQueue({concurrency: 1});
 const transactions: any[] = [];
 const operations: any[] = [];
@@ -15,48 +16,48 @@ let lines = 0;
 
 async function insert() {
     if ( transactions.length === 0 ) return;
-    // console.log(`Inserting ${transactions.length} transactions...`);
-    // console.log(response);
     console.log(`Inserting ${transactions.length}/${inserts} transactions...`);
     await client.insert({table: "transactions", values: transactions, format: "JSONEachRow"})
     await client.insert({table: "operations", values: operations, format: "JSONEachRow"})
-    // console.log(response);
     transactions.length = 0;
     operations.length = 0;
 }
 
-console.log("Scanning inscriptions...");
-// console.time("Scan completed");
+export function scan() {
+    return new Promise((resolve, reject) => {
+        console.log("Scanning inscriptions...");
 
-const rl = readline.createInterface({
-    input: fs.createReadStream(TRANSACTIONS_PATH),
-    crlfDelay: Infinity
-});
+        const rl = readline.createInterface({
+            input: fs.createReadStream(TRANSACTIONS_PATH),
+            crlfDelay: Infinity
+        });
 
-rl.on('line', (line: string) => {
-    const row = JSON.parse(line);
-    // operations++;
-    lines++;
-    const opCode = parseOpCode(row.content_uri);
-    // console.log(opCode);
+        rl.on('line', (line: string) => {
+            const row = JSON.parse(line);
+            lines++;
+            const opCode = parseOpCode(row.content_uri);
 
-    queue.add(async () => {
-        inserts++;
-        transactions.push([row]);
-        operations.push({...opCode, id: row.transaction_hash});
-        if (transactions.length >= 1000) await insert();
+            queue.add(async () => {
+                inserts++;
+                transactions.push([row]);
+                operations.push({...opCode, id: row.transaction_hash});
+                if (transactions.length >= max_inserts) await insert();
+            });
+        });
+
+        rl.on("close", async () => {
+            console.log(`Total lines: ${lines}`);
+            console.log("Waiting for queue to empty..."); // "Waiting for queue to empty...
+            await queue.onEmpty();
+            console.log("Queue is empty");
+            await insert();
+            console.log("Done");
+            resolve(true);
+        });
+
+        rl.on("error", (error) => {
+            console.error(error);
+            reject(error);
+        });
     });
-});
-
-rl.on("close", async () => {
-    // console.timeEnd("Scan completed");
-    console.log(`Total lines: ${lines}`);
-    console.log("Waiting for queue to empty..."); // "Waiting for queue to empty...
-    await queue.onEmpty();
-    console.log("Queue is empty");
-    await insert();
-});
-
-rl.on("error", (error) => {
-    console.error(error);
-});
+}
