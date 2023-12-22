d4b7ef795f6f3a1c3a105326335d140893233ea6
Denis Carriere <carriere.denis@gmail.com>
2023-12-22 17:41:06 -0500

    update emitter

--- src/BlockEmitter.ts
+++ src/BlockEmitter.ts
@@ -1,8 +1,18 @@
 import { readPackage } from "@substreams/manifest";
 import { createRegistry, createRequest, applyParams } from "@substreams/core";
 import { BlockEmitter, createNodeTransport } from "@substreams/node";
-import { token, baseUrl, manifest, outputModule, params, startBlockNum, stopBlockNum, FINAL_BLOCKS_ONLY } from "./config.js";
+import { manifest, outputModule, params, startBlockNum, stopBlockNum, FINAL_BLOCKS_ONLY } from "./config.js";
 import { readCursor } from "./utils.js";
+
+// auth API token
+// https://app.streamingfast.io/
+// https://app.pinax.network/
+if (!process.env.SUBSTREAMS_API_TOKEN) {
+  throw new Error("SUBSTREAMS_API_TOKEN is require");
+}
+
+export const token = process.env.SUBSTREAMS_API_TOKEN;
+export const baseUrl = "https://eos.substreams.pinax.network:443";
 
 // Read Substream
 const substreamPackage = await readPackage(manifest);
@@ -17,7 +27,7 @@ const request = createRequest({
   outputModule,
   startBlockNum,
   stopBlockNum,
-  // startCursor: readCursor(),
+  startCursor: readCursor(),
   finalBlocksOnly: FINAL_BLOCKS_ONLY,
   // productionMode: true,
 });
--- src/config.ts
+++ src/config.ts
@@ -8,16 +8,6 @@ export const DEFAULT_DATABASE = "default";
 export const DEFAULT_USERNAME = "default";
 export const DEFAULT_PASSWORD = "";
 
-// auth API token
-// https://app.streamingfast.io/
-// https://app.pinax.network/
-if (!process.env.SUBSTREAMS_API_TOKEN) {
-    throw new Error("SUBSTREAMS_API_TOKEN is require");
-}
-
-export const token = process.env.SUBSTREAMS_API_TOKEN;
-export const baseUrl = "https://eos.substreams.pinax.network:443";
-
 // User parameters
 export const manifest = "https://github.com/pinax-network/substreams/releases/download/common-v0.6.0/common-v0.6.0.spkg";
 export const outputModule = "map_transaction_traces";
