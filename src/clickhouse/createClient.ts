8cdd9de31e69bc48341f5df571c770d348ec9469
Denis Carriere <carriere.denis@gmail.com>
2023-12-23 22:01:46 -0500

    fix pending

--- src/clickhouse/createClient.ts
+++ src/clickhouse/createClient.ts
@@ -1,22 +1,14 @@
-import { createClient as createClientWeb } from "@clickhouse/client-web";
+import { createClient } from "@clickhouse/client";
 import { DATABASE, HOST, PASSWORD } from "../config.js";
-import { ping } from "./ping.js";
 
-function createClient(readonly = false) {
-    const client = createClientWeb({
-        host: HOST,
-        password: PASSWORD,
-        database: DATABASE,
-        clickhouse_settings: {
-            wait_for_async_insert: 0, // 0
-            async_insert: 1, // 1
-            readonly: readonly ? "1" : "0",
-        },
-        application: "EORC-20 Indexer",
-    });
-    client.command = client.exec;
-    client.ping = ping as any;
-    return client;
-}
-
-export const client = createClient();
+export const client = createClient({
+    host: HOST,
+    password: PASSWORD,
+    database: DATABASE,
+    clickhouse_settings: {
+        wait_for_async_insert: 0, // 0
+        async_insert: 1, // 1
+        readonly: "0",
+    },
+    application: "EORC-20 Indexer",
+});
